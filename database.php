d4011421cf498886a8ecc941f0e94269a2d8761a
misaeldelima <misael27091997@gmail.com>
2020-07-03 21:27:09 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
     $hostname = "localhost"; //geralmente é localhost
-    $usuario = "root";
-    $senha = "";
-    $dbname = "mapos";
+    $usuario = "root"; //usuario do banco de dados
+    $senha = ""; //senha do banco de dados
+    $dbname = "mapos"; //nome do banco de dados
    //Criar conexão
    $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
